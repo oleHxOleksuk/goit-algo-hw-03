2a23dbc3ac1515f17895ec861a993eecd19ce7ca
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2024-12-08 13:50:47 +0200

    cort task_2

--- task_2.py
+++ task_2.py
@@ -1,11 +1,11 @@
 from random import randint
 
 def get_numbers_ticket(min:int, max:int, quantity:int):
-    if not (1<=min<max<=1000):
+    if not (1<=min<max<=1000) or (max - min) < quantity:
         return []
     lottery_numbers = set()
     while len (lottery_numbers) != quantity:
         lottery_numbers.add(randint(min, max))
     return list(lottery_numbers)
 
-print(get_numbers_ticket(1, 30, 10))
+print(get_numbers_ticket(1, 10, 9))
