1e6ec1822020671f7d89d52fe30c4d05c803b02c
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2024-12-08 14:25:17 +0200

    corect task_3

--- task_3.py
+++ task_3.py
@@ -1,23 +1,13 @@
 import re
 
-def normalize_phone_contactats(raw_numbers):
-    for number in raw_numbers:
-        clean_number = re.sub(r'\D', '', number)
-        if not clean_number.startswith('+'):
-            if clean_number.startswith('380'):
-                clean_number = '+' + clean_number
-            else:
-                clean_number = '+38' + clean_number
-        return clean_number
+def normalize_phone(phone_number:str):
+    sanitized_numbers = re.sub(r'\D', '', phone_number)
+    if not sanitized_numbers.startswith('+'):
+        if sanitized_numbers.startswith('380'):
+            sanitized_numbers = '+' + sanitized_numbers
+        else:
+            sanitized_numbers = '+38' + sanitized_numbers
+    return sanitized_numbers
 
 
-print(normalize_phone_contactats([
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",]))
+print(normalize_phone("067\\t123 4567     "))
